0c3afe9358c6a85809a181f5dc7c8cc6b648fab3
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-10-03 01:05:51 +0530

    uuls

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
 class Book(models.Model):
@@ -9,8 +10,10 @@ class Book(models.Model):
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
 
-    
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[self.pk])
 
     def __str__(self):
         return f"{self.title}({self.rating})"
         
+
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import  get_object_or_404, render
+from django.http import Http404
 
 from .models import Book
 
@@ -11,11 +12,15 @@ def index(request):
     })
 
 def book_detail(request, pk):
+    # try:
+    #   book = book.object.get(Book, pk=pk)
+    # except:  
+    #           raise Http404("")
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'book_outlet/book_detail.html', {
         "book": book,
         "title": book.title, 
         "author": book.author,
         "rating": book.rating,
-        "is_bestseller": book.is_bestselling
+        "is_bestseller": book.is_bestselling 
     })
