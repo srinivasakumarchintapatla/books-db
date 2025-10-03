818b63117e3f5fc5bb0e6412c7878ccf018412cd
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-10-03 12:22:59 +0530

    12:22

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -10,14 +10,15 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False) 
-    slug = models.SlugField(default="",null=False, db_index=True)
+    slug = models.SlugField(default="",null=False)
+    # slug = models.SlugField(default="",null=False, db_index=True)
      #harry-potter 1 => harry-potter-1
     def get_absolute_url(self):
         # return reverse("book_detail", args=[self.slug])
         return reverse("book_detail", args=[self.id])
     
     def save(self,*args, **kwargs):
-         self.slug = slugify(self.title) _
+         self.slug = slugify(self.title) 
          super().save(*args, **kwargs)
 
     def __str__(self):
