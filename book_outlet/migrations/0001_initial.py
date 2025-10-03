64b6716ab755899cbaf0892ce0bae0ea57a76751
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-10-03 17:16:53 +0530

    17:16

--- book_outlet/migrations/0001_initial.py
+++ book_outlet/migrations/0001_initial.py
@@ -18,9 +18,9 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('rating', models.IntegerField(validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(5)])),
-                ('author', models.CharField(max_length=100, null=True)),
-                ('is_bestselling', models.BooleanField(default=False)),
-                ('slug', models.SlugField(blank=True, default='')),
+                # ('author', models.CharField(max_length=100, null=True)),
+                # ('is_bestselling', models.BooleanField(default=False)),
+                # ('slug', models.SlugField(blank=True, default='')),
             ],
         ),
     ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import  get_object_or_404, render
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
 # Create your views here.
 
 def index(request):
-        books = Book.objects.all()
+        books = Book.objects.all().order_by('-rating')
+        num_books = books.count()
+        avg_rating = books.aggregate(Avg("rating"))#rating__avg, rating__max, rating__min
         return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "totlal_number_of_books": num_books,
+        "avarage_rating": avg_rating
     })
 
 def book_detail(request, pk): # Changed from slug to pk
@@ -16,7 +21,7 @@ def book_detail(request, pk): # Changed from slug to pk
     # This will query by ID (pk) and automatically raise a 404 if not found.
     # book = get_object_or_404(Book, slug=slug)
     book = get_object_or_404(Book, pk=pk)
-
+_
     return render(request, "book_outlet/book_detail.html", {
         "book": book,
         "title": book.title, 
