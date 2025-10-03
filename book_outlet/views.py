f58f3710f0d7ba1803f7acfdffcefb0d5d01bd6a
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-10-03 19:05:58 +0530

    19:05

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -21,7 +21,6 @@ def book_detail(request, pk): # Changed from slug to pk
     # This will query by ID (pk) and automatically raise a 404 if not found.
     # book = get_object_or_404(Book, slug=slug)
     book = get_object_or_404(Book, pk=pk)
-_
     return render(request, "book_outlet/book_detail.html", {
         "book": book,
         "title": book.title, 
